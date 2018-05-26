5f9bbabc0eab03d0ddec1418b286f9ee621dd737
UnderABloodySky <valenzuelahoracioe@gmail.com>
2018-05-26 06:00:03 -0300

    Borro algunos de los mensajes inservibles

--- Project/src/resultados/Resultado.java
+++ Project/src/resultados/Resultado.java
@@ -5,15 +5,8 @@ import oponentes.Oponente;
 
 
 public abstract class Resultado {
-
-	protected final Integer zero = new Integer(0);
-	protected final Integer one = new Integer(1);
 	
 		public abstract Oponente getGanador();
-	
-		public abstract Integer cantidadEmpates();
-	
-		public abstract Integer cantidadVictoria();
 
-	
+
 }
